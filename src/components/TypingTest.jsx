f78d297f0e2391820637c83243dc3c110015e64d
sogalabhi <abhijithsogal@gmail.com>
2024-10-10 04:40:38 +0530

    wpm, accuracy calculation and reset button

--- src/components/TypingTest.jsx
+++ src/components/TypingTest.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useRef, useState } from 'react'
 
 const TypingTest = () => {
     const paragraph = `According to the caption on the bronze marker placed by the Multnomah Chapter of the Daughters of the American Revolution on May 12, 1939, College Hall is the oldest building in continuous use for Educational purposes west of the Rocky Mountains. Here were educated men and women who have won recognition throughout the world in all the learned professions.`
-    const maxTime = 60
+    const maxTime = 10
     const [timeLeft, settimeLeft] = useState(maxTime)
     const [Mistakes, setMistakes] = useState(0)
     const [WPM, setWPM] = useState(0)
+    const [accuracy, setaccuracy] = useState(0)
     const [charIndex, setcharIndex] = useState(0)
     const [isTyping, setisTyping] = useState(false)
     const inputRef = useRef(null)
@@ -16,6 +17,31 @@ const TypingTest = () => {
         inputRef.current.focus()
         setCorrectwrong(Array(charRefs.current.length).fill(''))
     }, [])
+    useEffect(() => {
+        let interval;
+        if (isTyping && timeLeft > 0) {
+            interval = setInterval(() => {
+                settimeLeft(timeLeft - 1);
+                let correctChars = charIndex - Mistakes;
+                let totalTime = maxTime - timeLeft;
+
+                let wpm = Math.round((correctChars / 5 / totalTime) * 60);
+                wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+                setWPM(wpm)
+
+                let acc = Math.round((correctChars / charIndex) * 100);
+                setaccuracy(acc)
+            }, 1000);
+        }
+        else if (timeLeft === 0) {
+            clearInterval(interval)
+            setisTyping(false)
+        }
+        return () => {
+            clearInterval(interval)
+        }
+    }, [isTyping, timeLeft])
+
     const onKeyDown = (e) => {
         const characters = charRefs.current
         let currentChar = charRefs.current[charIndex]
@@ -54,6 +80,14 @@ const TypingTest = () => {
             }
         }
     }
+    const resetBtn = ()=>{
+        setisTyping(false)
+        settimeLeft(maxTime)
+        setcharIndex(0)
+        setMistakes(0)
+        setWPM(0)
+        setCorrectwrong(Array(charRefs.current.length).fill(''))
+    }
     return (
         <div>
 
@@ -70,7 +104,8 @@ const TypingTest = () => {
                 <p>Time left: <strong>{timeLeft}</strong></p>
                 <p>Mistakes: <strong>{Mistakes}</strong></p>
                 <p>WPM: <strong>{WPM}</strong></p>
-                <button className='bg-yellow-300 p-1 rounded '>Try again</button>
+                <p>Accuracy: <strong>{accuracy}</strong></p>
+                <button className='bg-yellow-300 p-1 rounded text-black' onClick={resetBtn}>Try again</button>
             </div>
         </div>
     )
